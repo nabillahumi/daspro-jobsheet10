69f8ea4009ab2029363f0b20fa003d19da5f5e89
nabillahumi <nabillahumi003@gmail.com>
2024-11-06 13:54:34 +0700

    Array 2 Jobsheet 10

--- SIAKAD18.java
+++ SIAKAD18.java
@@ -10,11 +10,14 @@ public class SIAKAD18 {
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke- " + (i+1));
-        
+            double totalPersiswa = 0;
+
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("Nilai mata kuliah " + (j+1) + " : ");
                 nilai[i][j] = sc.nextInt();
+                totalPersiswa += nilai[i][j];
             }
+            System.out.println("Nilai rata-rata : " + totalPersiswa/3);
         }
     }
 }
