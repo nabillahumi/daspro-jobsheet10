a6cfda49e5fe27a6c02646dcc39e72742ef301c2
nabillahumi <nabillahumi003@gmail.com>
2024-11-12 14:41:10 +0700

    Array 2 Jobsheet 10

--- BiskopWithScanner18.java
+++ BiskopWithScanner18.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-;
 
 public class BiskopWithScanner18 {
     
@@ -12,6 +11,8 @@ public class BiskopWithScanner18 {
         int baris, kolom;
 
         while (true) { 
+
+            //input data penonton
             System.out.print("Masukkan nama : ");
             nama = sc.nextLine();
             System.out.print("Masukkan baris : ");
@@ -19,15 +20,25 @@ public class BiskopWithScanner18 {
             System.out.print("Masukkan kolom : ");
             kolom = sc.nextInt();
             sc.nextLine();
-
+            
             penonton[baris-1][kolom-1] = nama;
 
-            System.out.print("Input penonton lainnya? (y/n) : ");
+            System.out.print("Input Penonton lainnya? (y/n) : ");
             next = sc.nextLine();
 
             if (next.equalsIgnoreCase("n")) {
                 break;
             }
         }
+        System.out.println("=== daftar Penonton ===");
+            for (int i = 0; i < penonton.length; i++) {
+                for (int j = 0; j < penonton[i].length; j++) {
+                    if (penonton[i][j] != null) {
+                        System.out.printf("Baris %d, kolom %d: %s\n", i + 1, j + 1, penonton[i][j]);
+                    } else {
+                        System.out.printf("Baris %d, Kolom %d: Kosong\n", i + 1, j +1);
+                     }
+                }
+            } 
     }
 }
--- SIAKAD18.java
+++ SIAKAD18.java
@@ -6,29 +6,35 @@ public class SIAKAD18 {
         
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah siswa : ");
+        int jmlhSiswa = sc.nextInt();
 
-        for (int i = 0; i < nilai.length; i++) {
+        System.out.print("Masukkan jumlah mata kuliah : ");
+        int jmlhMatkul = sc.nextInt();
+
+        int[][] nilai = new int[jmlhSiswa][jmlhMatkul];
+
+        for (int i = 0; i < jmlhSiswa; i++) {
             System.out.println("Input nilai mahasiswa ke- " + (i+1));
             double totalPersiswa = 0;
 
-            for (int j = 0; j < nilai[i].length; j++) {
+            for (int j = 0; j < jmlhMatkul; j++) {
                 System.out.print("Nilai mata kuliah " + (j+1) + " : ");
                 nilai[i][j] = sc.nextInt();
                 totalPersiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata : " + totalPersiswa/3);
+            System.out.println("Nilai rata-rata : " + totalPersiswa/jmlhMatkul);
         }
         System.out.println("\n=====================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah : ");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jmlhMatkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jmlhSiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata Kuliah " + ( j+ 1) + " : " + totalPerMatkul / 4);
+            System.out.println("Mata Kuliah " + (j+ 1) + " : " + totalPerMatkul/jmlhSiswa);
         }
     }
 }
